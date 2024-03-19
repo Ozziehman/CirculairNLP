0267f039ce1064878c024d9802626a211ddfb9c2
MasterDisaster7 <45339840+MasterDisaster7@users.noreply.github.com>
2024-03-19 19:41:41 +0100

    added text block support

--- Utils/FileDataExtraction/parse_all_files.py
+++ Utils/FileDataExtraction/parse_all_files.py
@@ -1,7 +1,6 @@
 import os
 from muPDF_reader import muPDF_reader
 
-
 input_folder = "InputFiles"
 pdf_files = [f for f in os.listdir(input_folder) if f.endswith('.pdf')]
 for pdf_file in pdf_files:

79fc2feb0c7b470f5660f751de6c90695f453fb8
Doomay <doomie.doommasterr@gmail.com>
2024-04-10 15:21:20 +0200

    grammar check

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 # IMPORT EVERYTHING HERE
-from Utils.FileDataExtraction import muPDF_reader_layer
-from Utils.FileDataStorage import neo4j_layer
+#from Utils.FileDataExtraction import muPDF_reader_layer
+#from Utils.FileDataStorage import neo4j_layer
+import language_tool_python
 
-mprl = muPDF_reader_layer.muPDF_reader_layer()
-n4l = neo4j_layer.neo4j_layer()
+class Pipeline:
+    def __init__(self, mprl, n4l) -> None:
+        self.mprl = mprl
+        self.n4l = n4l
 
-mprl.ParseAllFiles("InputFiles") #parse all files in the input folder
-n4l.upload_all_files("OutputFiles", upload_text=True, upload_tables=True, upload_images=True, interpreted_structure=True) #upload all files in the output folder
-n4l.structurize_neo4j_database() #structurize the db
+    def execute(self) -> None:
+        # Parse all files in the input folder
+        self.mprl.parse_all_files("InputFiles")
 
+        # Upload all files in the output folder
+        self.n4l.upload_all_files("OutputFiles", upload_text=True, upload_tables=True, upload_images=True, interpreted_structure=True)
+
+        # Structurize the db
+        self.n4l.structurize_neo4j_database()
+
+class CoreferenceInContext:
+    def __init__(self) -> None:
+        pass
+
+    def input() -> str:
+        return input("Please enter your text: \n")
+    
+    def grammar_check(self) -> str:
+        input = CoreferenceInContext.input()
+        grammar_checker = language_tool_python.LanguageTool("en-US")
+        grammar_checker.close()
+        return print(grammar_checker.correct(input))
+
+
+if __name__ == "__main__":
+    #mprl = muPDF_reader_layer.muPDF_reader_layer()
+    #n4l = neo4j_layer.neo4j_layer()
+    
+    #pipeline = Pipeline(mprl, n4l)
+    coref_in_context = CoreferenceInContext()
+    # pipeline.execute()
+    coref_in_context.grammar_check()

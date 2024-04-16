a1e18eb1ca8e1d8bc14c0b28a8e2b51639a0d116
Oscar Theelen <oscartheelen@gmail.com>
2024-04-16 15:08:54 +0200

    fixed pipeline, + fixed requirements

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from Utils.FileDataStorage import neo4j_layer
 import language_tool_python
 
 class Pipeline:
-    def __init__(self, mprl, n4l) -> None:
-        self.mprl = mprl
-        self.n4l = n4l
+    def __init__(self) -> None:
+        self.mprl = muPDF_reader_layer.muPDF_reader_layer()
+        self.n4l = neo4j_layer.neo4j_layer()
 
     def execute(self) -> None:
         # Parse all files in the input folder
-        self.mprl.parse_all_files("InputFiles")
+        self.mprl.ParseAllFiles("InputFiles")
 
         # Upload all files in the output folder
         self.n4l.upload_all_files("OutputFiles", upload_text=True, upload_tables=True, upload_images=True, interpreted_structure=True)
@@ -19,8 +19,8 @@ class Pipeline:
         self.n4l.structurize_neo4j_database()
 
 class CoreferenceInContext:
-    def __init__(self, n4l) -> None:
-        self.n4l = n4l
+    def __init__(self) -> None:
+        self.n4l = neo4j_layer.neo4j_layer()
 
     def input(self) -> str:
         x = input("\nWhat's the name of the paragraph you wish to fetch? \nFor example: paragraph_1 \nInput: ")
@@ -35,14 +35,11 @@ class CoreferenceInContext:
 
 
 if __name__ == "__main__":
-    mprl = muPDF_reader_layer.muPDF_reader_layer()
-    n4l = neo4j_layer.neo4j_layer()
-    
     choice = input("Do you wish to load the full pipeline or apply coreference? \n 1 = Pipeline \n 2 = Coreference  \nInput: ")
     if choice == '1':
-        pipeline = Pipeline(mprl, n4l)
+        pipeline = Pipeline()
         pipeline.execute()
         print("Pipeline Executed")
     elif choice == '2':
-        coref_in_context = CoreferenceInContext(n4l)
+        coref_in_context = CoreferenceInContext()
         coref_in_context.grammar_check(coref_in_context.input())
